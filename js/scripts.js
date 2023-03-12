8a88ad30bb478602444be3f6099e153554cc7395
davidrmcneill <124522922+davidrmcneill@users.noreply.github.com>
2023-03-12 14:29:38 -0400

    Adding pokemonlist with array details

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList =[{name: 'Abra', height: .9, type: ['Normal', 'Rock']},{name: 'Arbok', height: 3.5, type: ['Rock', 'Ghost']},{name: 'Staryu', height: .8, type: ['Normal','Ghost']}]
