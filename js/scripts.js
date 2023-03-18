daabbf625f1134539865f294016cfad0b45679ea
davidrmcneill <124522922+davidrmcneill@users.noreply.github.com>
2023-03-18 13:33:14 -0400

    Added a for loop, and within it, a conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-let pokemonList =[{name: 'Abra', height: .9, type: ['Normal', 'Rock']},{name: 'Arbok', height: 3.5, type: ['Rock', 'Ghost']},{name: 'Staryu', height: .8, type: ['Normal','Ghost']}]
+let pokemonList = [
+    {Name: 'Abra', Height: .9, Type: ['Normal', 'Rock']},
+    {Name: 'Arbok', Height: 3.5, Type: ['Rock', 'Ghost']},
+    {Name: 'Staryu', Height: 1.8, Type: ['Normal','Ghost']}
+]
+
+let space = " ";
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].Height < 1) {
+        document.write(pokemonList[i].Name + ":" + space + "Height" + space + pokemonList[i].Height + space + "Small Pokemon" + space);
+    } else if (pokemonList[i].Height < 3 && pokemonList[i].Height > 1) {
+        document.write(pokemonList[i].Name + ":" + space + "Height" + space + pokemonList[i].Height + space + "Median Pokemon" + space);
+    } else {
+        document.write(pokemonList[i].Name + ":" + space + "Height" + space + pokemonList[i].Height + space + "Wow That Is Big!" + space);
+    }
+}
